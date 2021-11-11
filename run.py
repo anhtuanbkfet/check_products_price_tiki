f0f92a6262118feb0ec6c7f194c33cd222b42dfa
= <=>
2021-11-11 14:54:10 +0700

    Update parameter for run.py

--- run.py
+++ run.py
@@ -1,10 +1,35 @@
+import argparse
+
 from web_driver import init_browser
 from get_all_product_tiki import get_all_product_url
 from scraping_tiki_bot import search_function
 
-if __name__ == "__main__":
 
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', default= None,
+                    help='txt file store the products url')
+    return parser.parse_args()
+
+def do_work(args):
     browser = init_browser()
-    item_list = get_all_product_url(browser, 'https://tiki.vn/cua-hang/thiet-bi-y-te-gia-dinh-hme')
-    search_function(browser, item_list)
+
+    if args.file is not None:
+        with open(args.file, 'r') as rFile:
+            item_list = rFile.readlines()
+            rFile.close()
+    else: 
+        item_list = get_all_product_url(browser, 'https://tiki.vn/cua-hang/thiet-bi-y-te-gia-dinh-hme')
+        with open('product_list.txt', 'w') as wFile:
+            for item in item_list:
+                wFile.write(item+'\n')
+            wFile.close()
+        search_function(browser, item_list)
     browser.close()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    do_work(args)
+    
